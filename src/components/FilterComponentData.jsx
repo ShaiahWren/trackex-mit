ec7b21057e071660994e01333d4cf76750bd7c88
Shaiah Emigh-Doyle <64337863+ShaiahWren@users.noreply.github.com>
2021-04-27 11:07:59 -0400

    dataTable

---
+++ src/components/FilterComponentData.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FilterComponentData = () => {
+    title: "Filters",
+    options: [
+        {title: "Category", list: ['Eating Out', "Groceries", "Electronics"]}, { title: "Type", list: ["Income", "Expense"]}
+    ]
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default FilterComponentData
